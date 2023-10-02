57e7f4f57a30fa236377cbc0093aeae78272e610
FilmBee <92419399+FilmBee@users.noreply.github.com>
2023-10-02 17:39:33 +0600

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/MOGAMBO-XD/File-Sharing-Bot'>Click here</a>\n○ Channel : @Cybermusicproject\n○ Support Group : @CyberSupportGroup</b>",
+            text = f"<b>○ Creator : <a href='https://t.me/Film_Bee'>FilmBee</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Support: @Film_Bee</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
